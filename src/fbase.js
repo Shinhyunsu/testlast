a32804bde98b3df93adb6294efaf064eddb7b7b8
신현수 <sinhyeonsu@sinhyeonsuui-MacBook-Pro.local>
2021-10-28 09:28:41 +0900

    5.2

--- src/fbase.js
+++ src/fbase.js
@@ -5,13 +5,21 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
+    /*apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
 
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MEESSAGIN_ID,
-    appId: process.env.REACT_APP_APP_ID
+    appId: process.env.REACT_APP_APP_ID*/
+
+    apiKey: "AIzaSyDNVLoQFHxWkZM5n_iHeSnz_ObKIklncl4",
+    authDomain: "cryptoprice-9054d.firebaseapp.com",
+
+    projectId: "cryptoprice-9054d",
+    storageBucket: "cryptoprice-9054d.appspot.com",
+    messagingSenderId: "540400785718",
+    appId: "1:540400785718:web:b3b5004b9539823ab4bb97"
 };
 
 initializeApp(firebaseConfig);
