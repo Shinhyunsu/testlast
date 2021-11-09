d556991b7ffbafeecc6763a05eed88e49c9bbf5d
신현수 <sinhyeonsu@sinhyeonsuui-MacBook-Pro.local>
2021-11-09 22:28:27 +0900

    modify0.006

--- src/Lib/setupProxy.js
+++ src/Lib/setupProxy.js
@@ -4,7 +4,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(
         proxy('/api', {
-            target: 'https://api.kucoin.com/', // 비즈니스 서버 URL 설정
+            target: 'https://api.kucoin.com/',
             changeOrigin: true
         })
     );
